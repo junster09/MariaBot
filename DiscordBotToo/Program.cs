c77b360b32021a8c76b4f3e7a95e1bca511a9f89
junster09 <ajr2z2025@gmail.com>
2019-10-12 22:38:44 -0700

    Update Program.cs
    followed a tutorial

--- DiscordBotToo/Program.cs
+++ DiscordBotToo/Program.cs
@@ -6,7 +6,7 @@ namespace DiscordBotToo
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm following a tutorial eccks dee");
         }
     }
 }
